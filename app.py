05e039c6a777844dd662a7ed78a9094820da64e7
rachmi00 <r.agyingi@gmail.com>
2024-11-06 16:31:17 +0100

    integration test

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
